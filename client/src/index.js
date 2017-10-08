9a233b1fe98ec2e4c0ddcdfd9d82a10ce7b83ddc
jiganomics <clark@robojig.com>
2017-10-08 17:18:03 -0400

    Fix main app

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App3 from "./App3";
+import App from "./App";
 import "./index.css";
 import "../semantic/dist/semantic.min.css";
 
 ReactDOM.render(
-  <App3 />,
+  <App />,
   document.getElementById("root") // eslint-disable-line no-undef
 );
